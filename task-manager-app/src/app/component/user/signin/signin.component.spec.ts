1d90767b9ad6c54cb2acb3946f29eae107a86b08
simonNozaki <tldk1993@gmail.com>
2019-01-05 18:12:31 +0900

    [web]微修正

--- task-manager-app/src/app/component/user/signin/signin.component.spec.ts
+++ task-manager-app/src/app/component/user/signin/signin.component.spec.ts
@@ -1,11 +1,12 @@
-import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed, inject, fakeAsync, tick } from '@angular/core/testing';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatInputModule, MatIconModule, MatFormFieldModule, MatMenuModule  } from '@angular/material';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { SigninComponent } from './signin.component';
 import { SignupComponent } from '../signup/signup.component';
@@ -21,10 +22,13 @@ import { SigninService } from '../../../service/signin.service';
 import { SignoutService } from '../../../service/signout.service';
 import { CommonDeliveryService } from '../../../service/common-delivery.service';
 import { Router } from '@angular/router';
-import { Location } from "@angular/common";
+import { Location, APP_BASE_HREF } from "@angular/common";
 import { SpecInitializingStubRouter } from '../../../stub/spec-initializing-stub-router';
 import { StubUsersSigninResponse } from '../../../stub/stub-users-signin-response';
 import { StubUsersSigninRequest } from '../../../stub/stub-users-signin-request';
+import { SigninServiceStub } from '../../../stub/user/signin-service-stub';
+import { ConnectionBackend } from '@angular/http';
+import { of } from 'rxjs/observable/of';
 
 /**
  * 利用者認証コンポーネントのテストスイート
@@ -38,6 +42,8 @@ describe('SigninComponent', () => {
   let router: Router;
   let location: Location;
   let API_URL = "http://localhost:18080/api/v1/user/signin";
+  let injector = getTestBed();
+  let debugElement: DebugElement;
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -45,24 +51,30 @@ describe('SigninComponent', () => {
         imports : [ BrowserModule,HttpClientModule,WebModule,WebRoutingModule,FormsModule,ReactiveFormsModule,BrowserAnimationsModule,
             MatButtonModule,MatInputModule,MatIconModule,MatMenuModule,MatFormFieldModule,MatCheckboxModule,HttpClientTestingModule ],
         providers : [ TaskService,TaskLabelService,SignupService,SigninService,SignoutService,CommonDeliveryService,
-            { provide : Router,useClass : SpecInitializingStubRouter }
+            { provide : Router, useClass : SpecInitializingStubRouter },
+            { provide: APP_BASE_HREF, useValue : '/' },
+            { provide : SigninServiceStub, useClass : SigninServiceStub },
+            { provide: ConnectionBackend, useClass: MockBackend },
         ],
         schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents().then(() => {
-          let injector = getTestBed();
           fixture = TestBed.createComponent(SigninComponent);
           component = fixture.componentInstance;
           fixture.detectChanges();
           location = TestBed.get(Location);
           router = TestBed.get(Router);
-          // ルーティングスペックのデフォルトページを初期化します
-          router.initialNavigation();
           signinService = injector.get(SigninService);
           commondeliveryService = TestBed.get(CommonDeliveryService);
           httpMock = injector.get(HttpTestingController);
+          debugElement = fixture.debugElement;
       });
   }));
+
+  beforeEach(() => {
+      let signinServiceSpy = jasmine.createSpyObj("SigninServiceSpy", ["signin"]);
+      let signinSpy = signinServiceSpy.signin.and.returnValue(of(StubUsersSigninResponse[0]));
+  });
 
   afterEach(() => {
       httpMock.verify();
@@ -81,26 +93,25 @@ describe('SigninComponent', () => {
        * 有効なメールアドレスとパスワードで認証できることを確認する
        * 2-1
        */
-      it("2-1", () => {
+      it("2-1", fakeAsync(() => {
           component.ngOnInit();
           // 入力フォームのHTML要素を取得します
           component.signinForm.controls['emailControl'].setValue(StubUsersSigninRequest[0].email);
           component.signinForm.controls['passwordControl'].setValue(StubUsersSigninRequest[0].password);
-          // バリデーションを通せることを確認します
+          
+          // ボタンを押して、クリックイベントを実行
+          fixture.debugElement.query(By.css('button'))
+              .triggerEventHandler('click', null);
+
+          tick();
+          // 更新を検知(これがないとバリデーションチェックでエラーになる)
+          fixture.detectChanges();
+    
+          // アサーション
+          expect(component.signin).toHaveBeenCalled();
           expect(component.signinForm.valid).toBeTruthy();
-
-          // DTOにデータをバインド、清浄結果を取得できることを確認します
-          signinService.signin(StubUsersSigninRequest[0]).subscribe(res => {
-              expect(res).toEqual(StubUsersSigninResponse[0]);
-          });
-
-          // 認証の実行
-          component.signin();
-
-          // タスク一覧画面に遷移することを確認します。
           expect(location.path()).toBe('/task');
-
-      });
+      }));
 
   });
 
